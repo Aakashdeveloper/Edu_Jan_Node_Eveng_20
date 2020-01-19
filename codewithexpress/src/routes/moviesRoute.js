5dbe7cd912c0e914cb430680b15fdc9e3c1045c4
aakashDeveloper <ahanda205@gmail.com>
2020-01-19 17:44:12 +0000

    a

--- codewithexpress/src/routes/moviesRoute.js
+++ codewithexpress/src/routes/moviesRoute.js
@@ -1,45 +1,46 @@
 var express = require('express');
 var moviesRouter = express.Router();
-var fs = require('fs');
-
-var movies=[
-    {
-        "_id": "5ab12612f36d2879268f284a",
-        "name": "Black Panther",
-        "language": "ENGLISH",
-        "rate": 4.5,
-        "type": "Action Adventure Fantasy",
-        "imageUrl": "https://image.ibb.co/f0hhZc/bp.jpg"
-      },
-      {
-        "_id": "5ab12666f36d2879268f2902",
-        "name": "Death Wish",
-        "language": "ENGLISH",
-        "type": "Action Crime Thriller",
-        "rate": 3.2,
-        "imageUrl": "https://image.ibb.co/gC9PfH/dw.jpg"
-      },
-      {
-        "_id": "5ab12678f36d2879268f291d",
-        "name": "Coco",
-        "language": "ENGLISH",
-        "type": "Adventure Animation Family",
-        "rate": 5,
-        "imageUrl": "https://image.ibb.co/dQwWSx/coco.jpg"
-      }
-]
-
+var mongodb = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017"
 
 function router(menu){
 
     moviesRouter.route('/')
         .get( function(req,res){
-          res.render('movies',{title:'Movies Page',movie:movies,menu})
+            mongodb.connect(url, function(err,dc){
+                if(err){
+                    res.status(501).send('Error while connecting')
+                }else{
+                    var dbo = dc.db('classdatabase');
+                    dbo.collection('movies').find({}).toArray(function(err,data){
+                        if(err){
+                            res.status(501).send('Error fetching data')
+                        }else{
+                            res.render('movies',{title:'Movies Page',movie:data,menu})
+                        };
+                    });
+                };
+            });
     });
 
-    moviesRouter.route('/details')
+    moviesRouter.route('/details/:id')
         .get(function(req,res){
-            res.send('Movies Detail Page')
+            //var id = req.params.id;
+            var {id} = req.params
+            mongodb.connect(url, function(err,dc){
+                if(err){
+                    res.status(501).send('Error while connecting')
+                }else{
+                    var dbo = dc.db('classdatabase');
+                    dbo.collection('movies').findOne({_id:id}, function(err,data){
+                        if(err){
+                            res.status(501).send('Error fetching data')
+                        }else{
+                            res.render('moviesDetails',{title:'Movies Page',movie:data,menu})
+                        };
+                    });
+                };
+            });
     });
 
     return moviesRouter
@@ -47,25 +48,3 @@ function router(menu){
 
 
 module.exports = router;
-
-
-
-/*moviesRouter.route('/')
-    .get( function(req,res){
-        fs.readFile('db.json','utf-8',function(err,data){
-            if(err) throw err;
-            // res.send(data);
-            res.render('movies',{title:'Movies Page',movie:data})
-        })
-    });
-
-
-
-    function add(a,b){
-        return a+b
-    }
-
-    add(1,2)
-*/
-///
-// data:data   === data
--- codewithexpress/src/routes/productRoute.js
+++ codewithexpress/src/routes/productRoute.js
@@ -1,53 +1,32 @@
 var express = require('express');
 var productRouter = express.Router();
-var fs = require('fs');
-
-var prod =[
-    [
-        {
-          "_id": "5a05dacc734d1d68d42d31f3",
-          "productId": 1,
-          "productName": "Leaf Rake",
-          "productCode": "GDN-0011",
-          "releaseDate": "March 19, 2016",
-          "description": "Leaf rake with 48-inch wooden handle.",
-          "price": 19.95,
-          "starRating": 3.5,
-          "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-          "_id": "5a05daec734d1d68d42d32ca",
-          "productId": 2,
-          "productName": "Garden Cart",
-          "productCode": "GDN-0023",
-          "releaseDate": "March 18, 2016",
-          "description": "15 gallon capacity rolling garden cart",
-          "price": 32.99,
-          "starRating": 4.2,
-          "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        },
-        {
-          "_id": "5a05db08734d1d68d42d3300",
-          "productId": 3,
-          "productName": "Hammer",
-          "productCode": "TBX-0048",
-          "releaseDate": "May 21, 2016",
-          "description": "Curved claw steel hammer",
-          "price": 8.9,
-          "starRating": 4.8,
-          "imageUrl": "https://i.ibb.co/TrR7jkM/hammer.png"
-        }
-    ]
-]
-
+var mongodb = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017"
 
 function router(menu){
 
     productRouter.route('/')
         .get(function(req,res){
-            res.render('product',{title:'Products Page',prod:prod[0],
+            mongodb.connect(url, function(err,dc){
+                if(err){
+                    res.status(501).send('Error while connecting')
+                }else{
+                    var dbo = dc.db('classdatabase');
+                    dbo.collection('products').find({}).toArray(function(err,data){
+                        if(err){
+                            res.status(501).send('Error fetching data')
+                        }else{
+                            res.render('product',{title:'Products Page',prod:data,
                                 menu})
+                        };
+                    });
+                };
+            });
+            
         });
+
+
+
 
     productRouter.route('/details')
         .get(function(req,res){
@@ -57,17 +36,4 @@ function router(menu){
     return productRouter
 }
 
-
 module.exports = router;
-
-
-
-/*productRouter.route('/')
-    .get(function(req,res){
-        fs.readFile('prod.json','utf-8',function(err,data){
-            if(err) throw err;
-            //res.send(data);
-            res.render('product',{title:'Products Page',prod:prod})
-        })
-    });
-*/
